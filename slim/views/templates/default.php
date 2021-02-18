87f907ecb4564fd45e78fab52e43d4602b3f4d7b
Barry O Sullivan <barry@tercet.io>
2021-02-18 18:29:07 +0000

    Talk page has been migrated over to the new templte. Its looking good.

--- slim/views/templates/default.php
+++ slim/views/templates/default.php
@@ -1,69 +1,267 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <?php if (getenv('ENV') != "development") :?>
-        <!-- Global site tag (gtag.js) - Google Analytics -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-112076964-1"></script>
-        <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'UA-112076964-1');
-        </script>
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>
+        <?php if (isset($page)): ?>
+            <?php echo $page->title; ?>
+        <?php else: ?>
+            Barry O'Sullivan: Legacy Web App Specialist
         <?php endif; ?>
-        <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+    </title>
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <meta name="author" content="Barry O'Sullivan" />
 
-        <meta charset="utf-8">
-        <title><?php echo $page->title; ?></title>
-        <meta name="robots" content="index,follow" />
-        <link rel="stylesheet" href="/themes/kiss/css/kiss.css">
-        <link rel="stylesheet" href="/themes/kiss/css/prism.css">
+    <?php if (isset($article)) :?>
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@barryosull" />
+        <meta name="twitter:title" content="<?php echo $article->title ?>" />
+        <meta name="twitter:image" content="<?php echo $article->coverImage ?>" />
 
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-        <link rel="icon" href="favicon.ico" type="image/x-icon">
-        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
-        <link rel="manifest" href="/site.webmanifest">
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
-        <meta name="msapplication-TileColor" content="#da532c">
-        <meta name="theme-color" content="#ffffff">
+        <meta property="og:title" content="<?php echo $article->title ?>"/>
+        <meta property="og:image" content="<?php echo $article->coverImage ?>" />
+    <?php endif; ?>
 
-        <?php if (isset($article)) :?>
-            <meta name="twitter:card" content="summary" />
-            <meta name="twitter:site" content="@barryosull" />
-            <meta name="twitter:title" content="<?php echo $article->title ?>" />
-            <meta name="twitter:image" content="<?php echo $article->coverImage ?>" />
+    <link rel="stylesheet" href="https://unpkg.com/tailwindcss/dist/tailwind.min.css" />
+    <!--Replace with your tailwind.css once created-->
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700" rel="stylesheet" />
 
-            <meta property="og:title" content="<?php echo $article->title ?>"/>
-            <meta property="og:image" content="<?php echo $article->coverImage ?>" />
-        <?php endif; ?>
+    <style>
+        .gradient {
+            background: linear-gradient(105deg, #d56c33 0%, #ffd658 100%);
+        }
 
-    </head>
-    <body>
-        <nav class="topnav">
-            <ul>
-                <li class="first"><a href="/">barryosull.com</a></li>
-                <li<?php if($_SERVER['REQUEST_URI'] !== '/'): ?> class="active"<?php endif; ?>><a href="/blog">Blog</a></li>
-                <li><a href="/talks">Talks</a></li>
-                <li><a href="https://github.com/barryosull">Github</a></li>
-                <li class="last"><a href="https://twitter.com/barryosull">Twitter</a></li>
-                <li><a href="https://www.linkedin.com/in/barryosu/">LinkedIn</a></li>
+        table {
+            margin: 10px 0;
+        }
+
+        table th, table td {
+            text-align: left;
+            border-bottom: 1px solid #ddd;
+            padding: 2px 4px;
+        }
+
+    </style>
+</head>
+<body class="leading-normal tracking-normal text-white gradient" style="font-family: 'Source Sans Pro', sans-serif;">
+<!--Nav-->
+<nav id="header" class="fixed w-full z-30 top-0 text-white gradient">
+    <div class="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
+        <div class="pl-4 flex items-center">
+            <a class="toggleColour text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl" href="/">
+                <img alt="logo" class="h-8 fill-current inline" src="https://static.thenounproject.com/png/3331774-200.png"/>
+                Barry O'Sullivan
+            </a>
+        </div>
+        <div class="block lg:hidden pr-4">
+            <button id="nav-toggle" class="flex items-center p-1 text-pink-800 hover:text-gray-900 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+                <svg class="fill-current h-6 w-6" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <title>Menu</title>
+                    <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+                </svg>
+            </button>
+        </div>
+        <div class="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden mt-2 lg:mt-0 bg-white lg:bg-transparent text-black p-4 lg:p-0 z-20" id="nav-content">
+            <ul class="list-reset lg:flex justify-end flex-1 items-center">
+                <li class="mr-3">
+                    <a class="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4" href="/blog">Blog</a>
+                </li>
+                <li class="mr-3">
+                    <a class="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4" href="/talks">Talks</a>
+                </li>
+                <li class="mr-3">
+                    <a class="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4" href="https://github.com/barryosull">Github</a>
+                </li>
+                <li class="mr-3">
+                    <a class="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4" href="https://twitter.com/barryosull">Twitter</a>
+                </li>
+                <li class="mr-3">
+                    <a class="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4" href="https://www.linkedin.com/in/barryosu/">LinkedIn</a>
+                </li>
             </ul>
-        </nav>
-        <div class="content">
-            <article class="page">
-                <?php if(!empty($page->title)): ?>
-                    <h1><?php echo $page->title; ?></h1>
-                <?php endif; ?>
-                <div class="page-content">
-                    <?php echo (new Parsedown)->parse($page->content); ?>
-                </div>
-            </article>
+            <button
+                    id="navAction"
+                    class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
+                    onclick="location.href='mailto:barry@tercet.io';"
+            >
+                Contact me
+            </button>
+        </div>
+    </div>
+    <hr class="border-b border-gray-100 opacity-25 my-0 py-0" />
+</nav>
 
+<section class="bg-white border-b pt-16 pb-8">
+    <div class="container max-w-5xl mx-auto m-8">
+        <article class="text-black m-6">
+            <?php if(!empty($page->title)): ?>
+                <h1 class="font-bold text-2xl lg:text-4xl mb-6"><?php echo $page->title; ?></h1>
+            <?php endif; ?>
+            <div>
+                <?php echo (new Parsedown)->parse($page->content); ?>
+            </div>
+        </article>
+    </div>
+</section>
+
+<svg class="wave-top" viewBox="0 0 1439 147" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        <g transform="translate(-1.000000, -14.000000)" fill-rule="nonzero">
+            <g class="wave" fill="#f8fafc">
+                <path
+                        d="M1440,84 C1383.555,64.3 1342.555,51.3 1317,45 C1259.5,30.824 1206.707,25.526 1169,22 C1129.711,18.326 1044.426,18.475 980,22 C954.25,23.409 922.25,26.742 884,32 C845.122,37.787 818.455,42.121 804,45 C776.833,50.41 728.136,61.77 713,65 C660.023,76.309 621.544,87.729 584,94 C517.525,105.104 484.525,106.438 429,108 C379.49,106.484 342.823,104.484 319,102 C278.571,97.783 231.737,88.736 205,84 C154.629,75.076 86.296,57.743 0,32 L0,0 L1440,0 L1440,84 Z"
+                ></path>
+            </g>
+            <g transform="translate(1.000000, 15.000000)" fill="#FFFFFF">
+                <g transform="translate(719.500000, 68.500000) rotate(-180.000000) translate(-719.500000, -68.500000) ">
+                    <path d="M0,0 C90.7283404,0.927527913 147.912752,27.187927 291.910178,59.9119003 C387.908462,81.7278826 543.605069,89.334785 759,82.7326078 C469.336065,156.254352 216.336065,153.6679 0,74.9732496" opacity="0.100000001"></path>
+                    <path
+                            d="M100,104.708498 C277.413333,72.2345949 426.147877,52.5246657 546.203633,45.5787101 C666.259389,38.6327546 810.524845,41.7979068 979,55.0741668 C931.069965,56.122511 810.303266,74.8455141 616.699903,111.243176 C423.096539,147.640838 250.863238,145.462612 100,104.708498 Z"
+                            opacity="0.100000001"
+                    ></path>
+                    <path d="M1046,51.6521276 C1130.83045,29.328812 1279.08318,17.607883 1439,40.1656806 L1439,120 C1271.17211,77.9435312 1140.17211,55.1609071 1046,51.6521276 Z" opacity="0.200000003"></path>
+                </g>
+            </g>
+        </g>
+    </g>
+</svg>
+<section class="container mx-auto text-center py-6 mb-12">
+    <h1 class="w-full my-2 text-5xl font-bold leading-tight text-center text-white">
+        Expert help
+    </h1>
+    <div class="w-full mb-4">
+        <div class="h-1 mx-auto bg-white w-1/6 opacity-25 my-0 py-0 rounded-t"></div>
+    </div>
+    <h3 class="my-4 text-3xl leading-tight">
+        Have a codebase that everyone is afraid to touch?
+    </h3>
+    <button
+            class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
+            onclick="location.href='mailto:barry@tercet.io';"
+    >
+        Contact Me
+    </button>
+</section>
+<!--Footer-->
+<footer class="bg-white">
+    <div class="container mx-auto px-8">
+        <div class="w-full flex flex-col md:flex-row py-6">
+            <div class="flex-3 mb-6 text-black">
+                <a class="no-underline hover:no-underline font-bold text-xl lg:text-2xl" href="/">
+                    <img alt="logo" class="h-8 fill-current inline" src="https://static.thenounproject.com/png/3331774-200.png"/>
+                    Barry O'Sullivan
+                </a>
+            </div>
         </div>
-        <div class="footer">
-            <p>barryosull.com &copy; 2018</p>
-        </div>
-        <script src="/themes/kiss/js/prism.js"></script>
-    </body>
+    </div>
+</footer>
+
+<script>
+    var scrollpos = window.scrollY;
+    var header = document.getElementById("header");
+    var navcontent = document.getElementById("nav-content");
+    var navaction = document.getElementById("navAction");
+    var brandname = document.getElementById("brandname");
+    var toToggle = document.querySelectorAll(".toggleColour");
+
+    document.addEventListener("scroll", function () {
+        scrollpos = window.scrollY;
+        if (scrollpos > 10) {
+            enableWhiteHeader();
+        } else {
+            enableColouredHeader();
+        }
+    });
+
+    function enableWhiteHeader()
+    {
+        header.classList.add("bg-white");
+        header.classList.remove("gradient");
+        navaction.classList.remove("bg-white");
+        navaction.classList.add("gradient");
+        navaction.classList.remove("text-gray-800");
+        navaction.classList.add("text-white");
+        //Use to switch toggleColour colours
+        for (var i = 0; i < toToggle.length; i++) {
+            toToggle[i].classList.add("text-gray-800");
+            toToggle[i].classList.remove("text-white");
+        }
+        header.classList.add("shadow");
+        navcontent.classList.remove("bg-gray-100");
+        navcontent.classList.add("bg-white");
+    }
+
+    function enableColouredHeader()
+    {
+        header.classList.add("gradient");
+        header.classList.remove("bg-white");
+        navaction.classList.remove("gradient");
+        navaction.classList.add("bg-white");
+        navaction.classList.remove("text-white");
+        navaction.classList.add("text-gray-800");
+        //Use to switch toggleColour colours
+        for (var i = 0; i < toToggle.length; i++) {
+            toToggle[i].classList.add("text-white");
+            toToggle[i].classList.remove("text-gray-800");
+        }
+
+        header.classList.remove("shadow");
+        navcontent.classList.remove("bg-white");
+        navcontent.classList.add("bg-gray-100");
+    }
+
+</script>
+<script>
+
+    var navMenuDiv = document.getElementById("nav-content");
+    var navMenu = document.getElementById("nav-toggle");
+
+    document.onclick = toggleMenu;
+
+    function toggleMenu(e) {
+        var target = (e && e.target) || (event && event.srcElement);
+
+        if (checkParent(target, navMenuDiv)) {
+            return;
+        }
+
+        if (checkParent(target, navMenu)) {
+            if (navMenuDiv.classList.contains("hidden")) {
+                navMenuDiv.classList.remove("hidden");
+            } else {
+                navMenuDiv.classList.add("hidden");
+            }
+            return;
+        }
+
+        navMenuDiv.classList.add("hidden");
+    }
+
+    function checkParent(t, elm) {
+        while (t.parentNode) {
+            if (t === elm) {
+                return true;
+            }
+            t = t.parentNode;
+        }
+        return false;
+    }
+
+</script>
+<?php if (getenv('ENV') != "development") :?>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-112076964-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-112076964-1');
+    </script>
+<?php endif; ?>
+<script src="/themes/kiss/js/prism.js"></script>
+</body>
 </html>
