28c44c991c2b8073289d832652f6c2a69384cea1
Barry O Sullivan <barry@tercet.io>
2022-01-19 11:16:27 +0000

    Update default.php

--- slim/views/templates/default.php
+++ slim/views/templates/default.php
@@ -10,7 +10,7 @@
 <meta name="description" content="" />
 <meta name="keywords" content="" />
 <meta name="author" content="Barry O'Sullivan" />
-<link rel="stylesheet" href="https://unpkg.com/tailwindcss/dist/tailwind.min.css" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css" integrity="sha512-wnea99uKIC3TJF7v4eKk4Y+lMz2Mklv18+r4na2Gn1abDRPPOeef95xTzdwGD9e6zXJBteMIhZ1+68QC5byJZw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <link rel="stylesheet" href="https://unpkg.com/@tailwindcss/typography@0.2.x/dist/typography.min.css"/>
 <link rel="stylesheet" href="https://unpkg.com/prismjs@1.23.0/themes/prism-okaidia.css" />
 
